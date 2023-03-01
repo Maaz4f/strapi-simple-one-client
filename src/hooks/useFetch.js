ad00a3063f3a72c1b02dfa8efbb6593390e826d5
Maaz4f <114129442+Maaz4f@users.noreply.github.com>
2023-03-01 18:40:20 +0500

    Update useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,15 +2,15 @@ import { useEffect, useState } from "react";
 import { fetchDataFromApi } from "../utils/api";
 const useFetch = (endpoint) => {
     const [data, setData] = useState();
-
+   const makeApiCall = async () => {
+        const res = await fetchDataFromApi(endpoint);
+        setData(res);
+    };
     useEffect(() => {
         makeApiCall();
     }, [endpoint]);
 
-    const makeApiCall = async () => {
-        const res = await fetchDataFromApi(endpoint);
-        setData(res);
-    };
+    
 
     return { data };
 };
